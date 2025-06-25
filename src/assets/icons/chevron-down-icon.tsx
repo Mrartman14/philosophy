303523d5e15f286a80eb9654b9754871a55c5e0f
alexander.borisenk <alexander.borisenk@aliexpress.ru>
2025-06-25 21:55:36 +0300

    add pics

---
+++ src/assets/icons/chevron-down-icon.tsx
@@ -0,0 +1,5 @@
+export const ChevronDownIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width="1em" height="1em" viewBox="0 0 10 10" fill="none" {...props}>
+    <path d="M1 3.5L5 7.5L9 3.5" stroke="currentColor" strokeWidth="1.5" />
+  </svg>
+);
