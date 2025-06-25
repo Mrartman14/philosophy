91e0584c5ef6ea392243cac26e09f846173f0e96
alexander.borisenk <alexander.borisenk@aliexpress.ru>
2025-06-25 18:21:09 +0300

    add base path for assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 // import { PhilosophersTimeline } from "@/components/lessons-timeline/timeline";
 
 export default function Home() {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
   return (
     <div>
       <Image
-        // className="dark:invert"
-        src="/logo.png"
+        src={`${basePath}/logo.png`}
         alt="Logo"
         width={500}
         height={500}
