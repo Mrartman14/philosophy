44a029243269954d94514a75f1ea8a80a4736e7c
alexander.borisenk <alexander.borisenk@aliexpress.ru>
2025-06-25 21:55:36 +0300

    add pics

--- src/components/app/app-header.tsx
+++ src/components/app/app-header.tsx
@@ -10,6 +10,8 @@ import { NavigationMenu } from "@base-ui-components/react";
 import { structure } from "@/structure";
 import { Mention } from "../shared/mention";
 import { ThemeSelect } from "./theme-select";
+import { DropdownArrowIcon } from "@/assets/icons/arrow-icon";
+import { ChevronDownIcon } from "@/assets/icons/chevron-down-icon";
 
 export const AppHeader: React.FC = () => {
   const pathname = usePathname();
@@ -62,7 +64,7 @@ export const AppHeader: React.FC = () => {
               </NavigationMenu.Icon>
             </NavigationMenu.Trigger>
             <NavigationMenu.Content className={contentClassName}>
-              <ul className="grid grid-cols-1 gap-2 w-[90vw] md:w-[500px] max-h-[80vh] overflow-y-scroll">
+              <ul className="grid grid-cols-1 gap-2 w-[95vw] md:w-[500px] max-h-[80vh] overflow-y-scroll">
                 {Object.entries(groupedByChapter).map(([chapter, data]) => {
                   return (
                     <div
@@ -132,7 +134,7 @@ export const AppHeader: React.FC = () => {
           >
             <NavigationMenu.Popup className="w-full rounded bg-(--background) border border-(--border) data-[ending-style]:easing-[ease] relative h-[var(--popup-height)] origin-[var(--transform-origin)] transition-[opacity,transform,width,height,scale,translate] duration-[var(--duration)] ease-[var(--easing)] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[ending-style]:duration-150 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 min-[500px]:w-[var(--popup-width)] xs:w-[var(--popup-width)]">
               <NavigationMenu.Arrow className="flex transition-[left] duration-[var(--duration)] ease-[var(--easing)] data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180">
-                <ArrowSvg />
+                <DropdownArrowIcon />
               </NavigationMenu.Arrow>
               <NavigationMenu.Viewport className="relative h-full w-full overflow-hidden" />
             </NavigationMenu.Popup>
@@ -159,30 +161,3 @@ const contentClassName =
   "data-[ending-style]:data-[activation-direction=right]:translate-x-[-50%]";
 
 const linkCardClassName = "block no-underline hover:underline font-semibold";
-
-function ChevronDownIcon(props: React.ComponentProps<"svg">) {
-  return (
-    <svg width="10" height="10" viewBox="0 0 10 10" fill="none" {...props}>
-      <path d="M1 3.5L5 7.5L9 3.5" stroke="currentColor" strokeWidth="1.5" />
-    </svg>
-  );
-}
-
-function ArrowSvg(props: React.ComponentProps<"svg">) {
-  return (
-    <svg width="20" height="10" viewBox="0 0 20 10" fill="none" {...props}>
-      <path
-        d="M9.66437 2.60207L4.80758 6.97318C4.07308 7.63423 3.11989 8 2.13172 8H0V10H20V8H18.5349C17.5468 8 16.5936 7.63423 15.8591 6.97318L11.0023 2.60207C10.622 2.2598 10.0447 2.25979 9.66437 2.60207Z"
-        className="fill-(--background)"
-      />
-      <path
-        d="M8.99542 1.85876C9.75604 1.17425 10.9106 1.17422 11.6713 1.85878L16.5281 6.22989C17.0789 6.72568 17.7938 7.00001 18.5349 7.00001L15.89 7L11.0023 2.60207C10.622 2.2598 10.0447 2.2598 9.66436 2.60207L4.77734 7L2.13171 7.00001C2.87284 7.00001 3.58774 6.72568 4.13861 6.22989L8.99542 1.85876Z"
-        className="fill-(--border)"
-      />
-      <path
-        d="M10.3333 3.34539L5.47654 7.71648C4.55842 8.54279 3.36693 9 2.13172 9H0V8H2.13172C3.11989 8 4.07308 7.63423 4.80758 6.97318L9.66437 2.60207C10.0447 2.25979 10.622 2.2598 11.0023 2.60207L15.8591 6.97318C16.5936 7.63423 17.5468 8 18.5349 8H20V9H18.5349C17.2998 9 16.1083 8.54278 15.1901 7.71648L10.3333 3.34539Z"
-        className="fill-(--border)"
-      />
-    </svg>
-  );
-}
