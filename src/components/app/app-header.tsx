d7cf399daf5891e7dd8e454751b2e69bb60b8ba8
alexander.borisenk <alexander.borisenk@aliexpress.ru>
2025-06-27 00:06:54 +0300

    remove excess import

--- src/components/app/app-header.tsx
+++ src/components/app/app-header.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import groupBy from "lodash/groupBy";
-import { Fragment, useMemo } from "react";
+import { useMemo } from "react";
 import { usePathname } from "next/navigation";
 import { NavigationMenu } from "@base-ui-components/react";
 
