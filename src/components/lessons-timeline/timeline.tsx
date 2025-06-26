88bc3aec176bdb05ac6f4e34c0e94bc9cacbed64
alexander.borisenk <alexander.borisenk@aliexpress.ru>
2025-06-26 16:46:44 +0300

    render sections paths

--- src/components/lessons-timeline/timeline.tsx
+++ src/components/lessons-timeline/timeline.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import * as d3 from "d3";
-import React, { useEffect, useRef, useState } from "react";
+import groupBy from "lodash/groupBy";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 
+import { structure } from "@/utils/structure";
 import { PhilosopherView } from "./philosopher-view";
+import { getColorFromString } from "@/utils/get-color-from-str";
 
 export type Timeline = {
   name: string;
   from: number;
   to: number;
-  level: number;
   imageSrc?: string;
 };
 const data: Timeline[] = [
@@ -17,56 +19,48 @@ const data: Timeline[] = [
     name: "Пифагор",
     from: -570,
     to: -490,
-    level: 0,
     imageSrc: "/philosophers/pythagoras.jpg",
   },
   {
     name: "Парменид",
     from: -540,
     to: -470,
-    level: 0,
     imageSrc: "/philosophers/parmenides.jpg",
   },
   {
     name: "Гераклит",
     from: -544,
     to: -483,
-    level: 0,
     imageSrc: "/philosophers/heraclitus.jpg",
   },
   {
     name: "Зенон",
     from: -490,
     to: -425,
-    level: 0,
     imageSrc: "/philosophers/zenon.jpg",
   },
   {
     name: "Платон",
     from: -427,
     to: -347,
-    level: 0,
     imageSrc: "/philosophers/plato.jpg",
   },
   {
     name: "Аристотель",
     from: -384,
     to: -322,
-    level: 0,
     imageSrc: "/philosophers/aristotle.jpg",
   },
   {
     name: "Эпикур",
     from: -341,
     to: -270,
-    level: 0,
     imageSrc: "/philosophers/epicurus.jpg",
   },
   {
     name: "Сократ",
     from: -469,
     to: -399,
-    level: 0,
     imageSrc: "/philosophers/socrates.jpg",
   },
 ];
@@ -75,7 +69,6 @@ type PhilosophersTimelineProps = {
   height?: number;
   width?: number;
 };
-
 export const PhilosophersTimeline: React.FC<PhilosophersTimelineProps> = () => {
   const [{ height, width }, setSize] = useState({ width: 0, height: 0 });
   useEffect(() => {
@@ -105,6 +98,53 @@ export const PhilosophersTimeline: React.FC<PhilosophersTimelineProps> = () => {
     svg.call(zoom);
   }, []);
 
+  const d = useMemo(() => {
+    const coords = data.map((philosopher) => ({
+      ...philosopher,
+      x: Math.trunc(xScale((philosopher.from + philosopher.to) / 2)),
+      y: Math.trunc(height - 40),
+    }));
+
+    const groupedByChapter = groupBy(structure, (x) => x.section);
+
+    const paths = Object.entries(groupedByChapter).map(
+      ([ch, lessons], index) => {
+        const yGap = (index + 1) * 20;
+        let points: { point: { x: number; y: number }; lesson: string }[] = [
+          {
+            lesson: lessons[0]!.title,
+            point: { x: xScale(minYear), y: height - yGap },
+          },
+        ];
+
+        lessons
+          .toSorted((a, b) => a.order - b.order)
+          .forEach((lesson) => {
+            lesson.mentions.forEach((mention) => {
+              const point = coords
+                .filter((x) => x.name === mention)
+                .map(({ x, y }) => ({ x, y }));
+
+              points = points.concat(
+                point.map((x) => ({ point: x, lesson: lesson.title }))
+              );
+            });
+          });
+
+        const d = points
+          .flatMap(({ point }, i) =>
+            i === 0
+              ? `M${point.x} ${point.y - yGap}`
+              : `L${point.x} ${point.y - yGap}`
+          )
+          .join(" ");
+        return { d, color: getColorFromString(ch) };
+      }
+    );
+
+    return paths;
+  }, [height, minYear, xScale]);
+
   const tickStep = 50;
   const ticks = [];
   for (
@@ -126,10 +166,15 @@ export const PhilosophersTimeline: React.FC<PhilosophersTimelineProps> = () => {
                 x2={xScale(year)}
                 y1={height - 40}
                 y2={height - 30}
-                stroke="#2c6590"
+                stroke="var(--link)"
                 strokeWidth={1}
               />
-              <text x={xScale(year)} y={height - 15} textAnchor="middle">
+              <text
+                x={xScale(year)}
+                y={height - 15}
+                textAnchor="middle"
+                className="text-xs"
+              >
                 {year < 0 ? `−${Math.abs(year)}` : year}
               </text>
             </g>
@@ -140,14 +185,20 @@ export const PhilosophersTimeline: React.FC<PhilosophersTimelineProps> = () => {
             x2={xScale(maxYear)}
             y1={height - 40}
             y2={height - 40}
-            stroke="#2c6590"
+            stroke="var(--link)"
             strokeWidth={2}
           />
 
+          {d.map(({ color, d }) => (
+            <g key={color}>
+              <path d={d} stroke={color} strokeWidth={2} fill="none" />
+            </g>
+          ))}
+
           {data.map((philosopher) => (
             <PhilosopherView
-              scale={transform.k}
               key={philosopher.name}
+              scale={transform.k}
               x={Math.trunc(xScale((philosopher.from + philosopher.to) / 2))}
               y={Math.trunc(height - 40)}
               philosopher={philosopher}
