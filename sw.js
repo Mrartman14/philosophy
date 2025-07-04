051bc677e07afe8ab2e37641cb3430a962d947e2
Mrartman14 <Mrartman14@users.noreply.github.com>
2025-07-04 14:42:50 +0000

    deploy: 23e584c767d057d86f80f7af262e17808878d537

---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('push', function (event) {
+    if (event.data) {
+        const data = event.data.json()
+        const options = {
+            body: data.body,
+            icon: data.icon || '/icon.png',
+            // badge: '/icon.png',
+            vibrate: [100, 50, 100],
+            data: {
+                dateOfArrival: Date.now(),
+                primaryKey: '2',
+            },
+        }
+    event.waitUntil(self.registration.showNotification(data.title, options))
+  }
+})
+ 
+self.addEventListener('notificationclick', function (event) {
+    console.log('Notification click received.')
+    event.notification.close()
+    event.waitUntil(clients.openWindow(process.env.NEXT_PUBLIC_BASE_URL))
+    // event.waitUntil(clients.openWindow('<https://your-website.com>'))
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+})
