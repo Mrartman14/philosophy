bc716c90b8cf6b1c7af21883b75ba37ceb994760
alexander.borisenk <alexander.borisenk@aliexpress.ru>
2025-07-06 23:58:39 +0300

    add caching logic

---
+++ src/app/offline/page.tsx
@@ -0,0 +1,11 @@
+import { OfflineIcon } from "@/assets/icons/offline-icon";
+
+export default function Page() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <h1 className="text-5xl font-bold flex items-center gap-4">
+        Нет сети <OfflineIcon />
+      </h1>
+    </div>
+  );
+}
---
+++ todo.md
@@ -0,0 +1,3 @@
+navigator.vibrate пока не работает на IOS. проверять не добавили ли поддержку
+navigator.setAppBadge тоже самое
+File System Access API работает в IOS в режиме изолированной песочницы
