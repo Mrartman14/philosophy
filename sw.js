32b39fdb405ecf621db2396c7cac20901e53f765
Mrartman14 <Mrartman14@users.noreply.github.com>
2025-07-07 07:47:26 +0000

    deploy: f53ba568d753e4d6490a9aeea3787f3d65652d24

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
 const STATIC_CACHE = 'static-v1';
 const NEXT_ASSETS_CACHE = 'next-assets-v1';
 const DOCX_CACHE = 'docx-v1';
+// const PAGE_DATA_CACHE = 'page-data-v1';
+const LECTURES_PAGE_CACHE = 'lecture-pages-v1';
 
 const STATIC_ASSETS = [
   '/',
   '/logo.png',
+  '/page-data.json',
   '/favicon.ico',
   '/index.html',
   '/offline.html',
   '/manifest.webmanifest',
-  // Добавьте сюда другие статические файлы, если нужно
 ];
 
 // Кэшируем только статические ассеты при установке
@@ -43,6 +45,19 @@ self.addEventListener('activate', event => {
 // Основная обработка fetch-запросов
 self.addEventListener('fetch', event => {
   const { request } = event;
+
+  // -1. lectures page content
+  if (request.url.includes('/lectures') && (request.url.endsWith('.html') || request.url.endsWith('.txt'))) {
+    event.respondWith(staleWhileRevalidate(request, LECTURES_PAGE_CACHE));
+    return;
+  }
+
+  // 0. page-data: stale-while-revalidate
+  // включить это когда получение жсона будет осуществляться не через файловую систему а через фетч
+  // if (request.url.endsWith('page-data.json')) {
+  //   event.respondWith(staleWhileRevalidate(request, PAGE_DATA_CACHE));
+  //   return;
+  // }
 
   // 1. DOCX-файлы: stale-while-revalidate
   if (request.url.endsWith('.docx')) {
@@ -99,18 +114,18 @@ async function staleWhileRevalidate(request, cacheName) {
 
 /** PUSHES */
 self.addEventListener('push', function (event) {
-    if (event.data) {
-        const data = event.data.json()
-        const options = {
-            body: data.body,
-            icon: data.icon || '/icon.png',
-            // badge: '/icon.png',
-            vibrate: [100, 50, 100],
-            data: {
-                dateOfArrival: Date.now(),
-                primaryKey: '2',
-            },
-        }
+  if (event.data) {
+    const data = event.data.json()
+    const options = {
+      body: data.body,
+      icon: data.icon || '/icon.png',
+      // badge: '/icon.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2',
+      },
+    }
     event.waitUntil(self.registration.showNotification(data.title, options))
   }
 })
