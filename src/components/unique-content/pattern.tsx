1be782bb1408e5abcd3fdbc4c5f69e5b878712bb
alexander.borisenk <alexander.borisenk@aliexpress.ru>
2025-09-18 10:59:39 +0300

    add lesson 44 note

--- src/components/unique-content/pattern.tsx
+++ src/components/unique-content/pattern.tsx
@@ -1,7 +1,7 @@
 type TestProps = object;
 const Pattern: React.FC<TestProps> = () => {
   return (
-    <div className="fixed top-0 left-0 bottom-0 right-0 w-full h-full pattern-lines pattern-blue-500 pattern-bg-white pattern-size-6 pattern-opacity-20" />
+    <div className="fixed -z-10 top-0 left-0 bottom-0 right-0 w-full h-full pattern-lines pattern-blue-500 pattern-bg-white pattern-size-6 pattern-opacity-20" />
   );
 };
 
