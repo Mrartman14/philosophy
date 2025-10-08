c83b71d8a91df0d6bdbb6ab0873b708610c05229
alexander.borisenk <alexander.borisenk@aliexpress.ru>
2025-10-08 12:10:23 +0300

    fix yandex metrics

--- src/components/yandex-metrika/yandex-metrika.tsx
+++ src/components/yandex-metrika/yandex-metrika.tsx
@@ -1,30 +1,70 @@
+/* eslint-disable */
+// @ts-nocheck
 "use client";
 
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
-import ym, { YMInitializer } from "react-yandex-metrika";
+// import ym, { YMInitializer } from "react-yandex-metrika";
 
-const YM_COUNTER_ID = 104376496;
+const YM_COUNTER_ID = 104376496 as const;
 export const YandexMetrika: React.FC = () => {
   const pathname = usePathname();
 
   useEffect(() => {
-    if (pathname) {
-      ym("hit", pathname);
-    }
-  }, [pathname]);
-
-  return (
-    <YMInitializer
-      accounts={[YM_COUNTER_ID]}
-      options={{
-        defer: true,
-        webvisor: true,
+    if (!window.ym) {
+      (function (m: any, e, t, r, i, k, a) {
+        m[i] =
+          m[i] ||
+          function () {
+            (m[i].a = m[i].a || []).push(arguments);
+          };
+        m[i].l = 1 * (new Date() as any);
+        (k = e.createElement(t)),
+          (a = e.getElementsByTagName(t)[0]),
+          (k.async = 1),
+          (k.src = r),
+          a.parentNode.insertBefore(k, a);
+      })(
+        window,
+        document,
+        "script",
+        "https://mc.yandex.ru/metrika/tag.js",
+        "ym"
+      );
+      window.ym(YM_COUNTER_ID, "init", {
         clickmap: true,
         trackLinks: true,
         accurateTrackBounce: true,
-      }}
-      version="2"
-    />
-  );
+        webvisor: true,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (window.ym && pathname) {
+      window.ym(YM_COUNTER_ID, "hit", pathname);
+    }
+  }, [pathname]);
+
+  return null;
+
+  // useEffect(() => {
+  //   if (pathname) {
+  //     ym(YM_COUNTER_ID, "hit", pathname);
+  //   }
+  // }, [pathname]);
+
+  // return (
+  //   <YMInitializer
+  //     accounts={[YM_COUNTER_ID]}
+  //     options={{
+  //       defer: true,
+  //       webvisor: true,
+  //       clickmap: true,
+  //       trackLinks: true,
+  //       accurateTrackBounce: true,
+  //     }}
+  //     version="2"
+  //   />
+  // );
 };
