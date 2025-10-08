462b578a4434af2ce73b79cdd38cf93346c28923
Mrartman14 <Mrartman14@users.noreply.github.com>
2025-10-08 09:11:51 +0000

    deploy: c83b71d8a91df0d6bdbb6ab0873b708610c05229

--- _next/static/OQr0rJgpRN6BJvixB9UQC/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fexams\u002F[slug]","\u002Flectures\u002F[slug]","\u002Flectures\u002F[slug]\u002Fsources\u002F[source]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
