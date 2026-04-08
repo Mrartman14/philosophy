b042b9f6de9624a11218d27347132ea6d2d8613b
alexander.borisenk <alexander.borisenk@aliexpress.ru>
2026-04-08 12:51:53 +0300

    feat(pwa): integrate UpdatePrompt + InstallBanner, remove SWProvider from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { SWProvider } from "@/app/_providers/sw-provider";
 import { AppPageProvider } from "./_providers/app-page-provider";
 import { AppHeader } from "@/components/app/app-header/app-header";
 import { AppFooter } from "@/components/app/app-footer/app-footer";
+import { InstallBanner } from "@/components/app/install-banner";
+import { UpdatePrompt } from "@/components/app/update-prompt";
 import { YandexMetrika } from "@/components/yandex-metrika/yandex-metrika";
 
 const geistSans = Geist({
@@ -59,14 +60,15 @@ export default function RootLayout({
           ${finalClasses}
           `}
       >
-        <SWProvider />
         <AppPageProvider>
           <AppHeader />
+          <InstallBanner />
           <main className="w-[100vw] max-w-[100vw] lg:w-full lg:max-w-screen-lg flex flex-col items-center md:border-l md:border-r md:border-(--border)">
             {children}
           </main>
           <AppFooter />
         </AppPageProvider>
+        <UpdatePrompt />
         <Suspense>
           <YandexMetrika />
         </Suspense>
